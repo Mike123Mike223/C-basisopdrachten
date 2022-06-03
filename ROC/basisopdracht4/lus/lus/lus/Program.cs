da9e1a32ab75953021c4bf00cd1ad2672ff88565
Mike123Mike223 <105846613+Mike123Mike223@users.noreply.github.com>
2022-06-03 10:00:41 +0200

    Add files via upload

---
+++ ROC/basisopdracht4/lus/lus/lus/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lus
+{
+    internal class Program
+    {
+
+
+        public static void Main(string[] args)
+        {
+
+            int n = 10;
+            int w = 0;
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+
+            {
+                Console.Write(w + ":  ");
+                w++;
+                for (int j = 0; j <= i; j++)
+                {
+
+                    if (j % 2 != 0)
+                    {
+
+                        Console.Write("odd (" + j.ToString() + ") ");
+                    }
+                    else
+                    {
+
+                        Console.Write("even (" + j.ToString() + ") ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            for (int i = n; i >= 0; i--)
+            {
+                Console.Write(w + ": ");
+                w++;
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.Write("odd (" + j.ToString() + ") ");
+                    }
+                    else
+                    {
+                        Console.Write("even (" + j.ToString() + ") ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ ROC/basisopdracht5/showfolders/showfolders/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace showfolders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (string entry in Directory.GetDirectories(@"c:"))
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
